e5803cb0ed1735726bfc536fbc407e831bca4cce
bohdloss <unclejack388@gmail.com>
2020-09-02 11:56:37 +0200

    test (maybe merge conflict?)

---
+++ FuckUncleJack/src/main/java/com/bohdloss/fuckunclejack/main/Dimension2i.java
@@ -0,0 +1,16 @@
+package com.bohdloss.fuckunclejack.main;
+
+public class Dimension2i {
+
+public int width;
+public int height;
+	
+public Dimension2i(int width, int height) {
+	this.width=width;
+	this.height=height;
+}
+
+public int getWidth() {return width;}
+public int getHeight() {return height;}
+
+}
